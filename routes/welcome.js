3d07f33a305c2d57affac72cd3b648834dec22fa
Christopher Thornton <rmdirbin@gmail.com>
2014-07-12 14:08:00 -0700

    Navigating back and fourth now works!

--- routes/welcome.js
+++ routes/welcome.js
@@ -5,7 +5,7 @@ require('date-utils');
 
 
 exports.index = function(req, res) {
-  var today = Date.today();
+  var today = new Date();
   var currentWeek = req.query.date ? new Date(parseInt(req.query.date)) : today,
       nextWeek = currentWeek.getTime() + (604800 * 1000),
       lastWeek = currentWeek.getTime() - (604800 * 1000);
